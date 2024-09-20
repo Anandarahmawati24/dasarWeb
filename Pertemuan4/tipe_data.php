d32ad10c67f73efb12cef2af7f3d68581bfb6378
anandarahmawati24 <rahmawatiananda571@gmail.com>
2024-09-20 08:27:53 +0700

    praktikum 2 step 5

--- Pertemuan4/tipe_data.php
+++ Pertemuan4/tipe_data.php
@@ -12,4 +12,17 @@ echo "Variable a: {$d} <br>";
 echo "Variable a: {$e} <br>";
 
 var_dump($e);
+
+$nilaiMatematika = 5.1;
+$nilaiIPA = 6.7;
+$nilaiBahasaIndonesia = 9.3;
+
+$rataRata = ($nilaiMatematika + $nilaiIPA +$nilaiBahasaIndonesia) / 3;
+
+echo "Matematika: {$nilaiMatematika} <br>";
+echo "IPA: {$nilaiIPA} <br>";
+echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
+echo "Rata-rata: {$rataRata} <br>";
+
+var_dump($rataRata);
 ?>
