124f660ce184f3310a984bbe6025f80aff16018f
anandarahmawati24 <rahmawatiananda571@gmail.com>
2024-09-26 14:46:42 +0700

    fungsi yang mengembalikan nilai soal no 4

--- Pertemuan5/fungsi.php
+++ Pertemuan5/fungsi.php
@@ -26,4 +26,11 @@ $ucapanSalam = "Selamat pagi";
 
 //memanggil lagi tanpa mengisi parameter salam
 perkenalan($saya);
+
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+
+echo "Umur saya adalah ". hitungUmur(2005, 2024) ." tahun";
 ?>
