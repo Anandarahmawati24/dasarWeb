b55336c0b457e63425680b14c81094138d7efed8
anandarahmawati24 <rahmawatiananda571@gmail.com>
2024-09-26 14:38:40 +0700

    fungsi dengan parameter soal no 2

--- Pertemuan5/fungsi.php
+++ Pertemuan5/fungsi.php
@@ -1,10 +1,29 @@
 <?php
-function perkenalan(){
+function perkenalan1(){
     echo "Assalamualaikum, ";
     echo "Perkenalkan, nama saya Nanda<br/>";
     echo "Senang berkenalan dengan anda</br>";
 }
 
 //memanggil fungsi yang sudah dibuat
-perkenalan();
+perkenalan1();
+
+//membuat fungsi
+function perkenalan($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+
+}
+
+//memanggil fungsi yang sudah dibuat
+perkenalan("Hamdana", "Halo");
+
+echo "<hr>";
+
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+
+//memanggil lagi
+perkenalan($saya,$ucapanSalam);
 ?>
