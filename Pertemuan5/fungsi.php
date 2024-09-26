900a87aa01e5d355b0388c4ed9f0191ad55149b5
anandarahmawati24 <rahmawatiananda571@gmail.com>
2024-09-26 14:28:35 +0700

    fungsi soal nomor 1

---
+++ Pertemuan5/fungsi.php
@@ -0,0 +1,10 @@
+<?php
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Nanda<br/>";
+    echo "Senang berkenalan dengan anda</br>";
+}
+
+//memanggil fungsi yang sudah dibuat
+perkenalan();
+?>
