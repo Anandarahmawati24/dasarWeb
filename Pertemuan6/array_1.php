5cf9b630c7e09bf97a18ce2b217575e603da0b76
anandarahmawati24 <rahmawatiananda571@gmail.com>
2024-10-04 08:09:31 +0700

    praktikum 1

---
+++ Pertemuan6/array_1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <h2>Array Terindeks</h2>
+        <?php
+        $Listdosen=["Eok Nur Hamdana", "Unggul Pamenang", "Bagas Nugaraha"];
+
+        echo $Listdosen[2] . "<br>";
+        echo $Listdosen[0] . "<br>";
+        echo $Listdosen[1] . "<br>";
+        ?>
+    </body>
+</html>
