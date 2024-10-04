af1c345c543d47b5e96d59bd7b28287e4f614dd5
anandarahmawati24 <rahmawatiananda571@gmail.com>
2024-10-04 08:13:05 +0700

    praktikum 1 soal no 1

--- Pertemuan6/array_1.php
+++ Pertemuan6/array_1.php
@@ -5,11 +5,11 @@
     <body>
         <h2>Array Terindeks</h2>
         <?php
-        $Listdosen=["Eok Nur Hamdana", "Unggul Pamenang", "Bagas Nugaraha"];
+        $Listdosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugaraha"];
 
-        echo $Listdosen[2] . "<br>";
-        echo $Listdosen[0] . "<br>";
-        echo $Listdosen[1] . "<br>";
+       foreach ($Listdosen as $dosen) {
+        echo $dosen . "<br>";
+       }
         ?>
     </body>
 </html>
