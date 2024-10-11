ee634e7a6bea7b875cb9f4a399b38aded7c6c6da
anandarahmawati24 <rahmawatiananda571@gmail.com>
2024-10-11 10:42:38 +0700

    praktikum 4 soal no 4.2

--- Pertemuan7/html_aman.php
+++ Pertemuan7/html_aman.php
@@ -9,15 +9,27 @@
         <label for="input">Masukkan sesuatu:</label>
         <input type="text" name="input" id="input" required><br><br>
 
+        <label for="email">Masukkan email:</label>
+        <input type="email" name="email" id="email" required><br><br>
+
         <input type="submit" value="Submit">
     </form>
 
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Mengamankan input teks
         $input = $_POST['input'];
         $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-
         echo "Input yang diterima: " . $input . "<br>";
+
+        // Memeriksa dan mengamankan input email
+        $email = $_POST['email'];
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $email = htmlspecialchars($email, ENT_QUOTES, 'UTF-8');
+            echo "Email yang diterima: " . $email;
+        } else {
+            echo "Input email tidak valid.";
+        }
     }
     ?>
 </body>
