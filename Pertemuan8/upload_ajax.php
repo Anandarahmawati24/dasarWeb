3d7e3931f0a83533730e6abac92471fee2304438
anandarahmawati24 <rahmawatiananda571@gmail.com>
2024-10-19 18:36:05 +0700

    praktikum 3 soal no 11

--- Pertemuan8/upload_ajax.php
+++ Pertemuan8/upload_ajax.php
@@ -8,10 +8,10 @@ if (isset($_FILES['file'])) {
     $file_ext_array = explode('.', $file_name); // Pisahkan hasil explode ke variabel
     $file_ext = strtolower(end($file_ext_array)); // Panggil end() menggunakan variabel
 
-    $extensions = array("pdf", "doc", "docx", "txt");
+    $extensions = array("png", "jpg", "jpeg", "gif");
 
     if (in_array($file_ext, $extensions) === false) {
-        $errors[] = "Ekstensi file yang diizinkan adalah PDF, DOC, DOCX, atau TXT.";
+        $errors[] = "Ekstensi file yang diizinkan adalah png, jpeg, jpg, atau gif.";
     }
 
     if ($file_size> 2097152) {
